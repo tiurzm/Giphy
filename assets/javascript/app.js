2e5aa81f44f910de2d3fc5c14c3fadd1589d63b2
Tiur Mawaddah <tiurzm@gmail.com>
2019-10-08 12:53:24 -0500

    <comment>

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    var disney = ["Tinker Bell","Evil Queen", "Mickey Mouse", "Pocahontas", "Mother Gothel", "Pascal", "Mowgli", "Li Shang", "Megara", "Simba"]; 
+var disney = ["Tinker Bell","Evil Queen", "Mickey Mouse", "Pocahontas", "Mother Gothel", "Pascal", "Mowgli", "Violet Parr", "Megara", "Simba", "Robin Hood", "James P. Sullivan", "Marie", "Dory", "Nick Wilde", "Bing Bong"]; 
+    
     for(var i = 0; i < disney.length; i++){
         var newButton = $("<button>");
         newButton.attr("data-character", disney[i])
@@ -8,16 +8,31 @@ $(document).ready(function(){
     }
     
 
-    $("button").on("click", function(){
+    $("button").on("click", function(event){
+        event.preventDefault();
         var character = $(this).attr("data-character");
-        console.log(character);
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + character + "&api_key=VVy1qq3CVz412GDiIaDHXKBkxXzX14kh&limit=10";
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + character + "&api_key=VVy1qq3CVz412GDiIaDHXKBkxXzX14kh&limit=10";
     
         $.ajax({
             url: queryURL,
             method: "GET"
         }) .then(function(response){
-            console.log(response);
+            for (var j = 0; j < response.data.length; j++) {
+                var url = response.data[j].images.fixed_height.url
+                var rating =response.data[j].rating
+                var p = $("<p>");
+                p.addClass("left");
+                p.text("Rating: " + rating);
+                var image = $("<img>");
+                image.attr("src", url);
+                $("#animate").prepend(p, image);
+            }
         });
+        $("#animate").empty();
     });
-});
+
+    $("#add-character").on("click", function(event){
+        event.preventDefault();
+        var value = $(".character-input").val().trim();
+        console.log(value);
+    });
