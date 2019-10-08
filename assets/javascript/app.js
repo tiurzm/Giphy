d88a23c1f160a1d90ddcfd67d6b04a32b9f34205
Tiur Mawaddah <tiurzm@gmail.com>
2019-10-08 18:40:31 -0500

    <COMMENT>

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,16 @@
 var disney = ["Tinker Bell","Evil Queen", "Mickey Mouse", "Pocahontas", "Mother Gothel", "Pascal", "Mowgli", "Violet Parr", "Megara", "Simba", "Robin Hood", "James P. Sullivan", "Marie", "Dory", "Nick Wilde", "Bing Bong"]; 
+    function addNew(){
+        for(var i = 0; i < disney.length; i++){
+            var newButton = $("<button>");
+            newButton.attr("data-character", disney[i])
+            newButton.text(disney[i]);
+            $("#character-button").append(newButton);
+        }
+    };
+    addNew();
     
-    for(var i = 0; i < disney.length; i++){
-        var newButton = $("<button>");
-        newButton.attr("data-character", disney[i])
-        newButton.text(disney[i]);
-        $("#character-button").append(newButton);
-    }
-    
-
-    $("button").on("click", function(event){
+    function addImage(){
+        $("button").on("click", function(event){
         event.preventDefault();
         var character = $(this).attr("data-character");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + character + "&api_key=VVy1qq3CVz412GDiIaDHXKBkxXzX14kh&limit=10";
@@ -20,19 +22,40 @@ var disney = ["Tinker Bell","Evil Queen", "Mickey Mouse", "Pocahontas", "Mother 
             for (var j = 0; j < response.data.length; j++) {
                 var url = response.data[j].images.fixed_height.url
                 var rating =response.data[j].rating
+                console.log(response); 
                 var p = $("<p>");
-                p.addClass("left");
                 p.text("Rating: " + rating);
+                p.addClass("text");
                 var image = $("<img>");
+                image.addClass("gif");
                 image.attr("src", url);
-                $("#animate").prepend(p, image);
+                image.attr("name", [j]);
+                $("#animate").append(p, image);
             }
         });
         $("#animate").empty();
-    });
+        });
+    }
+    addImage();
+    
 
     $("#add-character").on("click", function(event){
         event.preventDefault();
-        var value = $(".character-input").val().trim();
-        console.log(value);
+        var value = $("#character-input").val().trim();
+        disney.push(value);
+        console.log(disney);
+        $("#character-button").empty();
+        addNew();
+        addImage();
     });
+
+    // $(".gif").on("click", function() {
+    //     var state = $(this).attr("data-state");
+    //     if (state === "still") {
+    //       $(this).attr("src", $(this).attr("data-animate"));
+    //       $(this).attr("data-state", "animate");
+    //     } else {
+    //       $(this).attr("src", $(this).attr("data-still"));
+    //       $(this).attr("data-state", "still");
+    //     }
+    // });
