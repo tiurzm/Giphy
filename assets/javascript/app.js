fe967065c253fc932f015cad5d42c76b27c8bb1e
Tiur Mawaddah <tiurzm@gmail.com>
2019-10-09 11:41:05 -0500

    <COMMENT>

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 var disney = ["Tinker Bell","Evil Queen", "Mickey Mouse", "Pocahontas", "Pascal", "Princess Jasmine","Mowgli", "Violet Parr", "Megara", "Simba", "Robin Hood", "James P. Sullivan", "Marie", "Dory", "Nick Wilde", "Mother Gothel"]; 
-    function addNew(){
+    function addAray(){
         for(var i = 0; i < disney.length; i++){
             var newButton = $("<button>");
             newButton.attr("data-character", disney[i])
@@ -7,61 +7,61 @@ var disney = ["Tinker Bell","Evil Queen", "Mickey Mouse", "Pocahontas", "Pascal"
             $("#character-button").append(newButton);
         }
     };
-    addNew();
+    addAray();
    
     function addImage(){
         $("button").on("click", function(event){
         event.preventDefault();
         var character = $(this).attr("data-character");
-        // var still = $(this).attr("data-still");
-        // var animate
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + character + "&api_key=VVy1qq3CVz412GDiIaDHXKBkxXzX14kh&limit=10";
     
         $.ajax({
             url: queryURL,
             method: "GET"
         }) .then(function(response){
-            console.log(response.data);
+            // console.log(response.data);
             for (var j = 0; j < response.data.length; j++) {
-                var url = response.data[j].images.fixed_height.url
-                console.log(url);
-                var still = response.data[j].images.fixed_height_still.url
-                console.log(still);
+                var move = response.data[j].images.fixed_height.url
+                // console.log(move);
+                var notMove = response.data[j].images.fixed_height_still.url
+                // console.log(notMove);
                 var rating =response.data[j].rating
                 var p = $("<p>");
                 p.text("Rating: " + rating);
                 p.addClass("text");
                 var image = $("<img>");
                 image.addClass("gif");
-                image.attr("src", still)
-                image.attr("src", url);
-                image.attr("name", [j]);
+                image.attr("data-animate", move);
+                image.attr("data-state", "still")
+                image.attr("data-still", notMove);
+                image.attr("src", notMove)
                 $("#animate").append(p, image);
             }
+            $(".gif").on("click",function(event){
+                event.preventDefault();
+                var state = $(this).attr("data-state");
+                if (state === "still") {
+                    $(this).attr("src", $(this).attr("data-animate"));
+                    $(this).attr("data-state", "animate");
+                  } else {
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still");
+                  }
+            })
         });
         $("#animate").empty();
         });
     }
     addImage();
     
-
+    
     $("#add-character").on("click", function(event){
         event.preventDefault();
         var value = $("#character-input").val().trim();
         disney.push(value);
         console.log(disney);
         $("#character-button").empty();
-        addNew();
+        addAray();
         addImage();
     });
 
-    $(".gif").on("click", function() {
-        var state = $(this).attr("data-state");
-        if (state === "animate") {
-            $(this).attr("src", $(this).attr("data-still"));
-            $(this).attr("data-state", "still");
-        } else {
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).attr("data-state", "animate");
-        }
-    });
